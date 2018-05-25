d4212ccfd0bc18b6e11153d4a0bb1f01054c4f9b
Britton Baumann <brittonbaumann@gmail.com>
2018-05-25 10:56:16 -0500

    added transiton when key is press

--- drum_kit/drum_kit.js
+++ drum_kit/drum_kit.js
@@ -7,15 +7,21 @@
 window.addEventListener("keydown", function(e){
   const code = e.keyCode;
   const audio = document.querySelector('audio[data-key=' + '"' + code + '"' + ']');
-  let playing = null;
-
-
-  audio.play();
-
-
-  window.addEventListener("keydown", function(e){
-    audio.pause();
-
-  });
-
+  const key = document.querySelector('.key[data-key=' + '"' + code + '"' + ']');
+  key.classList.add('playing');
+  if (!audio) return;
+  audio.currentTime = 0;
 });
+
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return
+
+    this.classList.remove('playing');
+
+  }
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+  // audio.play();
